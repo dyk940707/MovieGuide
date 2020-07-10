73d87385b8039133b96aaa392be5ff4aaa9d6bbd
kdy <dykim9407@efit.co.kr>
2020-07-10 18:29:33 +0900

    feat: RxAlamofire를 이용한 API 통신 구현

---
+++ MovieGuide/MovieGuide/APIService/APIService.swift
@@ -0,0 +1,72 @@
+//
+//  APIService.swift
+//  MovieGuide
+//
+//  Created by FIT on 2020/07/10.
+//  Copyright © 2020 com.dy. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Foundation
+import RxAlamofire
+
+class APIService {
+
+    let disposeBag = DisposeBag()
+    static let share = APIService()
+    
+    func movieListUpdate(_ movieType: String) -> Observable<[MovieListModel.Result]> {
+        // 데이터 생산
+        let ob = RxAlamofire
+            .requestData(.get, "https://api.themoviedb.org/3/movie/\(movieType)?api_key=588c3d1360999056ac3c7d59dff46fcd&language=ko-KR&page=1")
+            .mapObject(type: MovieListModel.self)
+            .map { (model: MovieListModel) in
+                return model.results
+        }
+
+        return ob
+    }
+}
+
+extension ObservableType {
+
+    public func mapObject<T: Codable>(type: T.Type) -> Observable<T> {
+        return flatMap { data -> Observable<T> in
+            let responseTuple = data as? (HTTPURLResponse, Data)
+
+            guard let jsonData = responseTuple?.1 else {
+                throw NSError(
+                    domain: "",
+                    code: -1,
+                    userInfo: [NSLocalizedDescriptionKey: "Could not decode object"]
+                )
+            }
+
+            let decoder = JSONDecoder()
+            let object = try decoder.decode(T.self, from: jsonData)
+
+            return Observable.just(object)
+        }
+    }
+
+    public func mapArray<T: Codable>(type: T.Type) -> Observable<[T]> {
+        return flatMap { data -> Observable<[T]> in
+            let responseTuple = data as? (HTTPURLResponse, Data)
+
+            guard let jsonData = responseTuple?.1 else {
+                throw NSError(
+                    domain: "",
+                    code: -1,
+                    userInfo: [NSLocalizedDescriptionKey: "Could not decode object"]
+                )
+            }
+
+            let decoder = JSONDecoder()
+            let objects = try decoder.decode([T].self, from: jsonData)
+
+            return Observable.just(objects)
+        }
+    }
+}
