b4ba4e77e38d7a754547a6d8a9a1ecf1dd06961a
dykim <doyune0707@gmail.com>
2020-07-15 00:32:42 +0900

    refactor: 영화검색 테이블 뷰 셀 별도로 추가, 아울렛 연결 작업

---
+++ MovieGuide/MovieGuide/Cell/MovieSearchTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MovieGuide
+//
+//  Created by 김도윤 on 2020/07/14.
+//  Copyright © 2020 com.dy. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieDetailLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var movieStarLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
