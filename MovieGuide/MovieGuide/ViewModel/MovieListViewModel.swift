9e533c873e6f41911559aa525a4065d757b8efd1
dykim <doyune0707@gmail.com>
2020-07-15 00:35:40 +0900

    refactor: 영화리스트 뷰 모델의 검색 구현 부분 제거 (따로 추가)

--- MovieGuide/MovieGuide/ViewModel/MovieListViewModel.swift
+++ MovieGuide/MovieGuide/ViewModel/MovieListViewModel.swift
@@ -25,36 +25,20 @@ class MovieListViewModel: MovieListViewModelType {
 
     let segChangeInVM = PublishSubject<Int>()
     let movieListInVM = BehaviorSubject<[TestBox]>(value: [])
-    let searchChangeInVM = PublishSubject<String>()
 
     var fetchList: Observable<[TestBox]>
     
     var pageList = [TestBox]()
     var pageIndex: Int = 0
-    
-    var searchPageList = [TestBox]()
-    var searchPageIndex: Int = 0
-    
+
     var segmentChange: AnyObserver<Int>
-    
-    var searchChange: AnyObserver<String>
 
     //2. 옵저버로 만듬
 
     init() {
         fetchList = movieListInVM.asObservable()
         segmentChange = segChangeInVM.asObserver()
-        searchChange = searchChangeInVM.asObserver()
-            
-        
-        searchChangeInVM
-            .subscribeOn(MainScheduler.instance)
-            .subscribe(onNext: { query in
-                self.searchMovieList(query: query, forceUpdate: true)
-            })
-            .disposed(by: disposeBag)
-
-        
+    
         //3. 서브젝트를 구독하여 값변경 -> 리스트를 변환시켜줘야함
         segChangeInVM
             .subscribeOn(MainScheduler.instance)
@@ -102,29 +86,5 @@ class MovieListViewModel: MovieListViewModelType {
         })
         .disposed(by: self.disposeBag)
     }
-    
-    func searchMovieList(query: String, forceUpdate: Bool) {
-        if forceUpdate {
-            searchPageIndex = 0
-            searchPageList = []
-        }
-        
-        searchPageIndex += 1
-        APIService.share.movieSearchUpdate(query, page: searchPageIndex)
-            .map { list in
-                return list.map { result in
-                    TestBox(movieImg: result.posterURL,
-                            movieName: result.originalTitle,
-                            movieDetail: result.overview,
-                            releaseDate: result.releaseDate,
-                            movieStar: "\(result.voteAverage)")
-                }
-        }
-        .observeOn(MainScheduler.instance)
-        .subscribe(onNext: { list in
-            self.searchPageList.append(contentsOf: list)
-            self.movieListInVM.onNext(self.searchPageList)
-        })
-        .disposed(by: disposeBag)
-    }
+
 }
