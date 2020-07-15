d529c8a1f411475c41810a951844e60ac5c94c0d
kdy <dykim9407@efit.co.kr>
2020-07-15 18:28:46 +0900

    feat: 최근 검색어 Cell 생성

---
+++ MovieGuide/MovieGuide/Cell/SearchedListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchedListTableViewCell.swift
+//  MovieGuide
+//
+//  Created by FIT on 2020/07/15.
+//  Copyright © 2020 com.dy. All rights reserved.
+//
+
+import UIKit
+
+class SearchedListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
