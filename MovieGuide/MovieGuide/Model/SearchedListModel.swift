cdc7469fd1cc282ca895331670d76aa007912c81
kdy <dykim9407@efit.co.kr>
2020-07-15 18:26:33 +0900

    feat: realm을 사용한 키워드 클래스 생성

---
+++ MovieGuide/MovieGuide/Model/SearchedListModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchedListModel.swift
+//  MovieGuide
+//
+//  Created by FIT on 2020/07/15.
+//  Copyright © 2020 com.dy. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class SearchedKeywordData: Object {
+    @objc dynamic var searchedKeyword = ""
+}
